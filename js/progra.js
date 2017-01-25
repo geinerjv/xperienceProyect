929fe8e4ab61acb3202756eebdb57ca4b162458e
Geiner Jiménez Villegas <linkmasck@gmail.com>
2017-01-25 16:55:05 -0600

    Queda terminada la sección de servicios

--- js/progra.js
+++ js/progra.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
   function mostrarMenu(){
     $("#btn-menu").click(function(){
       if (estado === 0) {
-        $("#nav-movil").slideDown();
+        $("#nav-movil").fadeIn();
         $("#btn-menu").removeClass("icon-menu");
         $("#btn-menu").addClass("icon-cross");
         estado = 1;
       }else {
-        $("#nav-movil").slideUp();
+        $("#nav-movil").fadeOut();
         $("#btn-menu").removeClass("icon-cross");
         $("#btn-menu").addClass("icon-menu");
         estado = 0;
@@ -33,7 +33,54 @@ $(document).ready(function(){
     })
   }
 
+  /*Este porcion de codigo permite desplazar suavemente las secciones de la página*/
+  /*Se seleccionan todos los elementos "a"*/
+  /*Con el "preventDefault" evitamos que el evento se active*/
+  $('a').click(function(e){
+    e.preventDefault();
+    $('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
+  });
 
+  /*
+  Porcion de codigo que permite al menu movil desaparecer despues de ser accionado alguno de sus items
+  */
+  $(".accionado-a").click(function(){
+    $("#nav-movil").fadeOut();
+    $("#btn-menu").removeClass("icon-cross");
+    $("#btn-menu").addClass("icon-menu");
+    estado = 0;
+  });
+
+
+  /*Este codigo es el que hace funcionar el boton de "ir arriba "*/
+  $('.ir-arriba').click(function(){
+    $('body, html').animate({
+         scrollTop: '0px'
+    }, 600);
+  });
+
+
+  var scrollUsuario2;
+  function scrollUsuario(){
+
+    $(window).scroll(function(){
+      scrollUsuario2 = $(this).scrollTop();
+      if( $(this).scrollTop() > 0 ){
+        $('.ir-arriba').slideDown(300);
+      } else {
+        $('.ir-arriba').slideUp(300);
+        }
+    });
+  }
+  scrollUsuario();
+
+
+  /*Este codigo permite situar en pixeles correctamente el menu de servicios en tamaño movil*/
+  $('#serv-menu-movil').click(function(){
+		    $('body, html').animate({
+			       scrollTop: '918px'
+		    }, 600);
+	  });
 
 
 });
